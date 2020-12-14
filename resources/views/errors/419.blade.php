9a22ce49797c6bc254fc6c1ac768dd5bf4110e45
Leandro Bilello <bilello79@gmail.com>
2020-12-14 12:01:42 -0300

    edit mensajes de error

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Page Expired'))
+@section('title', __('Pagina expirada'))
 @section('code', '419')
-@section('message', __('Page Expired'))
+@section('message', __('Ha pasado mucho tiempo desde que se cargo esta página. Recarguela e intente nuevamente'))

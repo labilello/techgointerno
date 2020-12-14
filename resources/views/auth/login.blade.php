c455968a26f11c3f4fb05d35cab65a564d339d39
Leandro Bilello <bilello79@gmail.com>
2020-12-14 11:14:13 -0300

    add solicitud de nuevo usuario

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,12 +36,35 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-end mt-4" x-data="{ show: false }">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+{{--                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">--}}
+{{--                        {{ __('Solicitar un nuevo usuario') }}--}}
+{{--                    </a>--}}
+                <div class="">
+                    <button @click={show=true} type="button" class="underline text-sm text-gray-600 hover:text-gray-900">Solicitar nuevo usuario</Button>
+                </div>
+                <div x-show="show" tabindex="0" class="z-40 overflow-auto left-0 top-0 bottom-0 right-0 w-full h-full fixed">
+                    <div  @click.away="show = false" class="z-50 relative p-3 mx-auto my-0 max-w-full" style="width: 600px;">
+                        <div class="bg-white rounded shadow-lg border flex flex-col overflow-hidden">
+                            <button @click={show=false} class="fill-current h-6 w-6 absolute right-0 top-0 m-6 font-3xl font-bold">&times;</button>
+                            <div class="px-6 py-3 text-xl border-b font-bold">Solicitar un nuevo usuario</div>
+                            <div class="p-6 flex-grow text-xs sm:text-sm md:text-base">
+                                <p>Para solicitar un nuevo usuario debera completar el siguiente formulario <a class="uppercase underline text-red-600" href="https://forms.gle/6svaURcdtU2RwEU7A">(IR AL FORMULARIO)</a><br>Luego de completarlo, un administrador se contactará contigo informandote usuario y contraseña.</p>
+                            </div>
+                            <div class="px-6 py-3 border-t">
+                                <div class="flex justify-end">
+                                    <button @click={show=false} type="button" class="bg-gray-700 text-gray-100 rounded px-4 py-2 mr-1">Cerrar</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="z-40 overflow-auto left-0 top-0 bottom-0 right-0 w-full h-full fixed bg-black opacity-50"></div>
+                </div>
 
                 <x-jet-button class="ml-4">
                     {{ __('Login') }}

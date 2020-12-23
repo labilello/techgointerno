7c958e33279ba2a0b92f08f24ec3c9aed9ef77ae
Leandro Bilello <bilello79@gmail.com>
2020-12-23 17:40:29 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
 
 
         @livewireStyles
+	<!-- Scripts -->
+        	<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.6.0/dist/alpine.js" defer></script>
+
     </head>
     <body class="font-opensans antialiased">
         <div class="min-h-screen bg-gray-200">
@@ -32,18 +35,15 @@
         @stack('modals')
 
         @livewireScripts
-        <!-- Scripts -->
-        <!-- Grab Google CDNs jQuery; fall back to local if necessary -->
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.6.0/dist/alpine.js" defer></script>
-        <script>window.Alpine || document.write('<script src="js/app.js"><\/script>')</script>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-{{--        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>--}}
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
         <script>window.bootstrap || document.write('<script src="js/app.js"><\/script>')</script>
 
-{{--        <script src="{{ asset('js/app.js') }}"></script>--}}
+        <script src="{{ asset('js/app.js') }}"></script>
         <script src="{{ asset('js/script.js') }}"></script>
     </body>
 </html>
